2143cab9cfefc2092cf7481e72f244e7ec46cd24
TheDanishMexican <danieljchristjansen@gmail.com>
2024-06-20 14:22:08 +0200

    finished a dialog

--- src/components/AddParticipantDialog.tsx
+++ src/components/AddParticipantDialog.tsx
@@ -2,8 +2,15 @@ import { useEffect, useState } from 'react'
 import Discipline from '../interfaces/Disciplines'
 import { fetchDisciplines } from '../services/fetchUtils'
 import '../styling/addParticipantDialog.css'
+import { Fade } from '@mui/material'
 
-export default function AddParticipantDialog() {
+export default function AddParticipantDialog({
+    isOpen,
+    toggleDialog,
+}: {
+    isOpen: boolean
+    toggleDialog: () => void
+}) {
     const [disciplines, setDisciplines] = useState<Discipline[]>([])
 
     async function fetchItems() {
@@ -16,34 +23,81 @@ export default function AddParticipantDialog() {
     }, [])
 
     return (
-        <dialog className="add-participant-dialog" open>
-            <input
-                className="input-field"
-                name="name"
-                placeholder="Name"
-                type="text"
-            />
-            <input
-                className="input-field"
-                name="age"
-                placeholder="Age"
-                type="number"
-            />
-            <input
-                className="input-field"
-                name="club"
-                placeholder="Club"
-                type="text"
-            />
-            <div className="add-participant-gender-input">
-                <label>
-                    <input type="radio" name="gender" />
-                    Male
-                </label>
-                <label>
-                    <input type="radio" name="gender" /> Female
-                </label>
-            </div>
-        </dialog>
+        <>
+            {isOpen && (
+                <Fade in={true} timeout={1000}>
+                    <dialog className="add-participant-dialog">
+                        <h2 className="add-participant-header">
+                            Add participant
+                        </h2>
+                        <div className="add-participant-dialog-content">
+                            <input
+                                className="input-field"
+                                name="name"
+                                placeholder="Name"
+                                type="text"
+                                required
+                            />
+                            <input
+                                className="input-field"
+                                name="age"
+                                placeholder="Age"
+                                type="number"
+                                required
+                            />
+                            <input
+                                className="input-field"
+                                name="club"
+                                placeholder="Club"
+                                type="text"
+                                required
+                            />
+                            <fieldset className="add-participant-gender-input">
+                                <legend>Gender</legend>
+                                <label>
+                                    <input
+                                        type="radio"
+                                        name="gender"
+                                        value="male"
+                                        required
+                                    />
+                                    Male
+                                </label>
+                                <label>
+                                    <input
+                                        type="radio"
+                                        name="gender"
+                                        value="female"
+                                    />
+                                    Female
+                                </label>
+                            </fieldset>
+                            <fieldset className="add-participant-discipline-input">
+                                <legend>Disciplines</legend>
+                                {disciplines.map((discipline) => (
+                                    <label key={discipline.id}>
+                                        <input
+                                            type="checkbox"
+                                            name="disciplines"
+                                            value={discipline.id}
+                                        />
+                                        {discipline.name}
+                                    </label>
+                                ))}
+                            </fieldset>
+                        </div>
+                        <div className="add-participant-dialog-buttons">
+                            <button className="simple-button">Add</button>
+                            <button
+                                onClick={toggleDialog}
+                                className="simple-button"
+                            >
+                                Cancel
+                            </button>
+                        </div>
+                    </dialog>
+                </Fade>
+            )}
+        </>
     )
 }
--- src/pages/ParticipantsPage.tsx
+++ src/pages/ParticipantsPage.tsx
@@ -1,24 +1,47 @@
+import { useState } from 'react'
 import ParticipantDialog from '../components/AddParticipantDialog'
 import ParticipantList from '../components/ParticipantList'
 import '../styling/participantPage.css'
 import { Fade } from '@mui/material'
 
 export default function ParticipantsPage() {
+    const [dialogOpen, setDialogOpen] = useState(false)
+
+    function toggleDialog() {
+        setDialogOpen((prev: boolean) => !prev)
+    }
+
     return (
-        <Fade in={true} timeout={1000}>
-            <div className="participants-page-container">
-                <h1 className="participants-page-header">Participants</h1>
-                <div className="participants-page-menu">
-                    <button className="simple-button">Add a participant</button>
-                    <input
-                        className="participant-search-input"
-                        type="text"
-                        placeholder="Search for participant"
+        <>
+            {dialogOpen && (
+                <div
+                    className="greyed-out-overlay"
+                    onClick={toggleDialog}
+                ></div>
+            )}
+            <Fade in={true} timeout={1000}>
+                <div className="participants-page-container">
+                    <h1 className="participants-page-header">Participants</h1>
+                    <div className="participants-page-menu">
+                        <button
+                            onClick={toggleDialog}
+                            className="simple-button"
+                        >
+                            Add a participant
+                        </button>
+                        <input
+                            className="participant-search-input"
+                            type="text"
+                            placeholder="Search for participant"
+                        />
+                    </div>
+                    <ParticipantList />
+                    <ParticipantDialog
+                        isOpen={dialogOpen}
+                        toggleDialog={toggleDialog}
                     />
                 </div>
-                <ParticipantList />
-                <ParticipantDialog />
-            </div>
-        </Fade>
+            </Fade>
+        </>
     )
 }
